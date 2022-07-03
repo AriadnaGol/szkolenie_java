d5795f3007bac473905e5959af4d272e688efaca
Ari <ariadna.golebicka@gmail.com>
2022-07-04 00:21:20 +0200

    implement adding and subtracting 2 numbers

--- MyFirstProject/src/Main.java
+++ MyFirstProject/src/Main.java
@@ -4,5 +4,14 @@ public class Main {
         String message2 = "Hello" + " world!";
         System.out.println(message);
         System.out.println(message2);
+
+        int a = 3;
+        int b = 6;
+        int c = a + b;
+        System.out.println("a + b = " + c);
+        c = a - b;
+        System.out.println("a - b = " + c);
+        c = b - a;
+        System.out.println("b - a = " + c);
     }
 }
