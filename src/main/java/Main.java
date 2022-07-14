240669c2a8a2441cdf5203a902b2fb8e26b741ed
AriadnaGol <ariadna.golebicka@gmail.com>
2022-07-15 00:44:18 +0200

    implement calculator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
 import cyber.trainees.homework.homework01.Homework01Service;
+import cyber.trainees.homework.homework02.*;
 
 public class Main {
     public static void main(String[] args) {
-        Homework01Service homework01Service = new Homework01Service();
-        homework01Service.printCalculations();
+        //Homework01Service homework01Service = new Homework01Service();
+        //homework01Service.printCalculations();
+
+        Calculator calculator = new Calculator(new Addition(), new Subtraction(), new Multiplication(), new Division());
+        calculator.printCalculations();
     }
 }
 
---
+++ src/main/java/cyber/trainees/homework/homework02/Division.java
@@ -0,0 +1,11 @@
+package cyber.trainees.homework.homework02;
+
+public class Division {
+    public double divide(int a, int b){
+        return (double)a / (double)b;
+    }
+
+    public double divide(double a, double b){
+        return a / b;
+    }
+}
