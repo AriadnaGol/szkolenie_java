b9b3a8786af3f2c2156c2d53fc24b98272a84369
AriadnaGol <ariadna.golebicka@gmail.com>
2022-07-16 14:06:11 +0200

    get vehicle model from file

--- src/main/java/cyber/trainees/service/ReadData.java
+++ src/main/java/cyber/trainees/service/ReadData.java
@@ -1,5 +1,10 @@
 package cyber.trainees.service;
 
+import cyber.trainees.inheritance.Color;
+import cyber.trainees.inheritance.VehicleModel;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class ReadData {
@@ -7,4 +12,28 @@ public class ReadData {
         Scanner scanner = new Scanner(System.in); //System.in to jest obiekt typu InputStream
         return scanner.nextLine();
     }
+
+    public String getDataFromFile(File externalFile) throws FileNotFoundException {
+        Scanner scanner = new Scanner(externalFile);
+        return scanner.nextLine();
+    }
+
+    public VehicleModel getVehicleModel(File externalFile) throws FileNotFoundException {
+        Scanner scanner = new Scanner(externalFile);
+        String line = scanner.nextLine();
+
+        String[] fields = line.split(",");
+
+        VehicleModel vehicleModel = new VehicleModel(
+                fields[0],
+                fields[1],
+                fields[2],
+                fields[3],
+                Integer.valueOf(fields[4]),
+                Double.valueOf(fields[5]),
+                Color.valueOf(fields[6]));
+
+        return vehicleModel;
+    }
 }
+
