3f590145d96e2f5ef780fda7c08a7c763b7c646b
AriadnaGol <ariadna.golebicka@gmail.com>
2022-07-16 08:41:38 +0200

    refactor print numbers remove new lines

--- src/main/java/cyber/trainees/spotkania/Loops.java
+++ src/main/java/cyber/trainees/spotkania/Loops.java
@@ -4,9 +4,9 @@ public class Loops {
     public void printNumbers(){
         for(int i = 1; i <= 100; i++) {
             if (i == 100) {
-                System.out.println(i);
+                System.out.print(i);
             } else {
-                System.out.println(i + ", ");
+                System.out.print(i + ", ");
             }
         }
     }
