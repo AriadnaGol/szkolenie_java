30cded4027f47f327c5e683777f1fa09f283581e
AriadnaGol <ariadna.golebicka@gmail.com>
2022-07-22 19:53:40 +0200

    remove content of main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,25 +6,8 @@ import cyber.trainees.inheritance.VehicleModel;
 
 public class Main {
     public static void main(String[] args) {
-        //Homework01Service homework01Service = new Homework01Service();
-        //homework01Service.printCalculations();
 
-        //Calculator calculator = new Calculator(new Addition(), new Subtraction(), new Multiplication(), new Division());
-        //calculator.printCalculations();
-
-        //Vehicle vehicle = new Vehicle("Volvo", "2022", "all wheels drive");
-        //System.out.println(vehicle.toString());
-
-        VehicleModel modelVehicle = new VehicleModel(
-                "Volvo",
-                "2022",
-                "all wheels drive",
-                "XC60",
-                1000,
-                2000.0,
-                Color.WHITE);
-
-        System.out.println(modelVehicle.toString());
     }
 }
+
 
