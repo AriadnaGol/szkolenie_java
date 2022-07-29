5ae5891656ce36a2a46d03e1261341167dd8839a
AriadnaGol <ariadna.golebicka@gmail.com>
2022-07-29 02:55:42 +0200

    solve homework for 23-07-2022

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,49 @@
-import cyber.trainees.homework.homework01.Homework01Service;
-import cyber.trainees.homework.homework02.*;
-import cyber.trainees.inheritance.Color;
-import cyber.trainees.inheritance.Vehicle;
-import cyber.trainees.inheritance.VehicleModel;
+import cyber.trainees.homework.homework03.Homework03;
+import cyber.trainees.wyjasnienia.Basics;
+import cyber.trainees.wyjasnienia.BasicsInteger;
 
 public class Main {
-    public static void main(String[] args) {
+    public static String SEPARATOR_SPACJA = "";
 
+    public String nazwaMetody() {
+        return "";
+    }
+
+    public Integer kolejnaMetoda() {
+        return 0;
+    }
+
+    public void wyswietlTekst(String tekst) {
+        System.out.println(tekst);
+    }
+
+
+    public static void main(String[] args) {
+        Homework03 homework03 = new Homework03();
+
+        //ZADANIE 1
+        var age = homework03.getAge();
+        System.out.println("Twoj wiek to jest: " + age);
+
+        //ZADANIE 2
+        var name = homework03.getName();
+        System.out.println("Twoje imie to jest: " + name);
+
+        //ZADANIE 3
+        System.out.println("Czy liczba 9 jest parzysta? " + homework03.isEven(9));
+        System.out.println("Czy liczba 6 jest parzysta? " + homework03.isEven(6));
+
+        //ZADANIE 4
+        System.out.println("Czy liczba 30 jest podzielna przez 3 i przez 5? " + homework03.isMultipleOf3And5(30));
+        System.out.println("Czy liczba 17 jest podzielna przez 3 i przez 5? " + homework03.isMultipleOf3And5(17));
+
+        //ZADANIE 5
+        System.out.println("Liczba 9 podniesiona do potegi 3 to jest: " + homework03.raiseToPowerOf3(9));
+
+        //ZADANIE 6
+        System.out.println("Pierwiastek kwadratowy z 16 to jest: " + homework03.getSqrtOfNumber(16.0));
     }
 }
+
 
 
