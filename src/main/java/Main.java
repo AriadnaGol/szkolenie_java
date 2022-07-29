35c4d80733944afdeb39994c40669f5a3ed09278
AriadnaGol <ariadna.golebicka@gmail.com>
2022-07-29 18:24:47 +0200

    solve exercise with big decimal list

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,21 @@ import cyber.trainees.homework.homework02.*;
 import cyber.trainees.inheritance.Color;
 import cyber.trainees.inheritance.Vehicle;
 import cyber.trainees.inheritance.VehicleModel;
+import cyber.trainees.wyjasnienia.Petle;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
+        Petle petle = new Petle();
 
+        var numbers = new ArrayList<BigDecimal>();
+        numbers.add(new BigDecimal(9));
+        numbers.add(new BigDecimal(3));
+        numbers.add(new BigDecimal(5));
+        petle.displayAllElements(numbers);
     }
 }
 
---
+++ src/main/java/cyber/trainees/wyjasnienia/Petle.java
@@ -0,0 +1,17 @@
+package cyber.trainees.wyjasnienia;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class Petle {
+    public void displayAllElements(List<BigDecimal> numbers) {
+        BigDecimal sum = new BigDecimal(0);
+
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.println(numbers.get(i));
+            sum = sum.add(numbers.get(i));
+        }
+
+        System.out.println("Suma liczb: " + sum);
+    }
+}
