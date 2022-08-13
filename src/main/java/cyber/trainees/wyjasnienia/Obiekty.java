e0e02abdef3486f0b9cc5404f5d5846d34625495
AriadnaGol <ariadna.golebicka@gmail.com>
2022-08-13 10:00:48 +0200

    introduce 3 cats

--- src/main/java/cyber/trainees/wyjasnienia/Obiekty.java
+++ src/main/java/cyber/trainees/wyjasnienia/Obiekty.java
@@ -3,13 +3,30 @@ package cyber.trainees.wyjasnienia;
 import cyber.trainees.wyjasnienia.Zwierzeta.Kot;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Obiekty {
 
     public void uruchom() {
-        Kot kot = new Kot("Kiara ", LocalDate.of(2022,4,18),1.9, "Wiki");
-        //kot.przedstawSie();
-        System.out.println(kot.przedstawSie());
+        List<Kot> koty = new ArrayList<>();
+
+        Kot kot1 = new Kot("Kiara ", LocalDate.of(2022,4,18),1.9, "Wiki");
+        koty.add(kot1);
+
+        Kot kot2 = new Kot("Kalhula ", LocalDate.of(2021,4,98),3.9, "Wiki");
+        koty.add(kot2);
+
+        Kot kot3 = new Kot("Kociuna ", LocalDate.of(2022,4,18),1.9, "Wiki");
+        koty.add(kot3);
+
+
+        for (Kot kot: koty) {
+            System.out.println(kot);
+        }
+
     }
+
+
 
 }
